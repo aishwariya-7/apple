44c1722248ff065429c87427a2aaccf767aeb297
aishwariya-7 <70738526+aishwariya-7@users.noreply.github.com>
2020-09-04 11:56:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+Str ="I LOVE BEEZ LAB";
+count =0;
+for(i in range(0.len(Str))):
+    if(Str[i]!=' '):
+        count=count+1;
+        print(Str(count));
